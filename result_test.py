9b78d548a28c5486a9c638d67dc7d202f1f0a9ed
roserland <1393745326@qq.com>
2020-05-12 21:44:52 +0800

    integrate three test in one

--- result_test.py
+++ result_test.py
@@ -10,6 +10,10 @@
 
 # merge 后的图片本地存储， 实验演示
 
+# 测试正确率？
+# CG-CNN, CG-CNN-RNN
+
+
 
 import pandas as pd
 import numpy as np
@@ -28,8 +32,16 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 import logging
 
+
 def main():
     demo_data = myDataset(csv_path='./coords/G_1_WSI.csv')
+
+    # load libraries
+    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.1, 0.1, 0.1])
+    trans = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
 
     demo_loader = torch.utils.data.DataLoader(
         demo_data,
@@ -50,6 +62,10 @@ def main():
 
     demo_probs = inference(1, demo_loader, model)
     demo_topk = group_argtopk(np.array(demo_data.slideIDX), demo_probs, 10)
+    demo_maxs = group_max(np.array(demo_data.slideIDX), demo_probs, len(demo_data.targets))
+    demo_pred = [1 if x >= 0.5 else 0 for x in demo_maxs]
+    print("demo_pred is ", demo_pred)
+    print("origin label:", demo_data.targets)
 
     demo_data.maketraindata(demo_topk)
 
@@ -60,7 +76,33 @@ def main():
     for i in range(len(demo_paths) // 10):
         merge_patchs(demo_paths[i*10:i*10 + 10])
 
-    print("demo finished")
+    print("****** demo finished *****\n")
+
+    # rnn test
+    rnn_demo_dset = rnndata(10, False, csv_path='./coords/G_1_WSI.csv', topk=demo_topk,
+                            transform=trans)
+    rnn_demo_loader = torch.utils.data.DataLoader(
+                        rnn_demo_dset,
+                        batch_size=10, shuffle=True,
+                        num_workers=2, pin_memory=False)
+    # make model
+    embedder = ResNetEncoder('./G_checkpoint_best.pth')
+    for param in embedder.parameters():
+        param.requires_grad = False
+    embedder = embedder.cuda()
+    embedder.eval()
+
+    rnn = rnn_single(128)
+    rnn = rnn.cuda()
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    val_loss, val_fpr, val_fnr = test_single(1, embedder, rnn, rnn_demo_loader, criterion)
+
+    print("In RNN test, error is {}".format(val_fnr + val_fpr))
+    print("RNN demo complete.\n")
+
+
+
 
 
 
@@ -82,8 +124,8 @@ def merge_patchs(grid_path, merge_dir = './demo_merges/'):
 
     res_img = Image.fromarray(res)
 
-    img_name = os.path.basename(grid_path[0])
-
+    img_name = os.path.basename(grid_path[0])[:23]
+    print("case {} is merging".format(img_name))
     res_img.save(img_name, 'jpg')
 
 
@@ -244,6 +286,201 @@ class myDataset(data.Dataset):
             return len(self.grid)
         elif self.mode == 2:
             return len(self.t_data)
+
+def train_single(epoch, embedder, rnn, loader, criterion, optimizer):
+    rnn.train()
+    running_loss = 0.
+    running_fps = 0.
+    running_fns = 0.
+
+    for i, (inputs, target) in enumerate(loader):
+        print('Training - Epoch: [{}/{}]\tBatch: [{}/{}]'.format(epoch + 1, args.nepochs, i + 1, len(loader)))
+
+        batch_size = inputs[0].size(0)
+        rnn.zero_grad()
+
+        state = rnn.init_hidden(batch_size).cuda()
+        for s in range(len(inputs)):
+            input = inputs[s].cuda()
+            _, input = embedder(input)
+            output, state = rnn(input, state)
+
+        target = target.cuda()
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * target.size(0)
+        fps, fns = errors(output.detach(), target.cpu())
+        running_fps += fps
+        running_fns += fns
+
+    running_loss = running_loss / len(loader.dataset)
+    running_fps = running_fps / (np.array(loader.dataset.targets) == 0).sum()
+    running_fns = running_fns / (np.array(loader.dataset.targets) == 1).sum()
+    running_err = (running_fns + running_fps) / 2
+    print('Training - Epoch: [{}/{}]\tLoss: {}\tFPR: {}\tFNR: {}\tERR: {}'.format(epoch + 1, args.nepochs, running_loss,
+                                                                         running_fps, running_fns, running_err))
+    return running_loss, running_fps, running_fns
+
+
+def test_single(epoch, embedder, rnn, loader, criterion):
+    rnn.eval()
+    running_loss = 0.
+    running_fps = 0.
+    running_fns = 0.
+
+    with torch.no_grad():
+        for i, (inputs, target) in enumerate(loader):
+            print('Validating - Epoch: [{}/{}]\tBatch: [{}/{}]'.format(epoch + 1, args.nepochs, i + 1, len(loader)))
+
+            batch_size = inputs[0].size(0)
+
+            state = rnn.init_hidden(batch_size).cuda()
+            for s in range(len(inputs)):
+                input = inputs[s].cuda()
+                _, input = embedder(input)
+                output, state = rnn(input, state)
+
+            target = target.cuda()
+            loss = criterion(output, target)
+
+            running_loss += loss.item() * target.size(0)
+            fps, fns = errors(output.detach(), target.cpu())
+            running_fps += fps
+            running_fns += fns
+
+    running_loss = running_loss / len(loader.dataset)
+    running_fps = running_fps / (np.array(loader.dataset.targets) == 0).sum()
+    running_fns = running_fns / (np.array(loader.dataset.targets) == 1).sum()
+    running_err = (running_fns + running_fps) / 2
+
+    print('Validating - Epoch: [{}/{}]\tLoss: {}\tFPR: {}\tFNR: {}\tERR: {}'.format(epoch + 1, args.nepochs, running_loss,
+                                                                           running_fps, running_fns, running_err))
+    return running_loss, running_fps, running_fns
+
+
+def errors(output, target):
+    _, pred = output.topk(1, 1, True, True)
+    pred = pred.squeeze().cpu().numpy()
+    real = target.numpy()
+    neq = pred != real
+    fps = float(np.logical_and(pred == 1, neq).sum())
+    fns = float(np.logical_and(pred == 0, neq).sum())
+    return fps, fns
+
+
+class ResNetEncoder(nn.Module):
+
+    def __init__(self, path):
+        super(ResNetEncoder, self).__init__()
+
+        temp = models.resnet34()
+        temp.fc = nn.Linear(temp.fc.in_features, 2)
+        ch = torch.load(path)
+        temp.load_state_dict(ch['state_dict'])
+        self.features = nn.Sequential(*list(temp.children())[:-1])
+        self.fc = temp.fc
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return self.fc(x), x
+
+
+class rnn_single(nn.Module):
+
+    def __init__(self, ndims):
+        super(rnn_single, self).__init__()
+        self.ndims = ndims
+
+        self.fc1 = nn.Linear(512, ndims)
+        self.fc2 = nn.Linear(ndims, ndims)
+
+        self.fc3 = nn.Linear(ndims, 2)
+
+        self.activation = nn.ReLU()
+
+    def forward(self, input, state):
+        input = self.fc1(input)
+        state = self.fc2(state)
+        state = self.activation(state + input)
+        output = self.fc3(state)
+        return output, state
+
+    def init_hidden(self, batch_size):
+        return torch.zeros(batch_size, self.ndims)
+
+class rnndata(data.Dataset):
+    def __init__(self, s, shuffle=False, csv_path='./coords/G_TwoTypes_Train.csv', topk = [],
+                 transform=None):
+        coords = pd.read_csv(csv_path)
+
+        assert len(topk) == 10*len(coords)
+
+        slides_path = coords['Path'].to_list()
+        slides_label = coords['TypeName'].to_list()
+
+        label_dict = {'LGG': 0, 'GBM': 1, }
+
+        wsi_level_label = [label_dict[x] for x in slides_label]
+
+        patch_level_label = []
+        grid = []
+        slide_IDX = []
+        for i, wsi_direc in enumerate(slides_path):
+            patch_name_list = os.listdir(wsi_direc)  # g is a slide directory path
+
+            # add prefix, so the patch has its full path
+            temp_full_path = [os.path.join(wsi_direc, x) for x in patch_name_list]
+
+            grid.extend(temp_full_path)
+            # patch label is WSI label
+            temp_label = label_dict[slides_label[i]]
+            patch_level_label.extend([temp_label] * len(patch_name_list))
+
+            slide_IDX.extend([i] * len(patch_name_list))
+
+        print('Number of tiles: {}'.format(len(grid)))
+
+        self.slide_names = slides_path
+        self.targets = wsi_level_label
+        self.s = s
+        self.shuffle = shuffle
+        self.transform = transform
+        self.grid = grid
+        self.patch_labels = patch_level_label
+        self.top_k = topk
+
+        print('loading finish, {} WSIs have been loaded'.format(len(self.targets)))
+
+    def __getitem__(self, index):
+        single_silde_path = self.slide_names[index]
+
+        grid_idx = self.top_k[index*10:index*10 + 10]
+
+        temp_grid = [self.grid[idx] for idx in grid_idx]
+
+        patch_name_list = os.listdir(single_silde_path)
+        # add prefix, so the patch has its full path
+        grid = [os.path.join(single_silde_path, x) for x in patch_name_list]
+
+        if self.shuffle:
+            temp_grid = random.sample(temp_grid, len(temp_grid))
+
+        out = []
+        s = min(self.s, len(grid))
+        for i in range(s):
+            img = Image.open(temp_grid[i])
+            img = img.resize((224, 224), Image.BILINEAR)
+            if self.transform is not None:
+                img = self.transform(img)
+            out.append(img)
+        return out, self.targets[index]
+
+
+    def __len__(self):
+        return len(self.targets)
 
 if __name__ == '__main__':
     main()
